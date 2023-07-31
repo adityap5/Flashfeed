28941276b3d119f33b41292be4439087dec48748
adityap5 <aditya29pippal@gmail.com>
2023-08-01 00:21:59 +0530

    cateo updated

--- src/App.js
+++ src/App.js
@@ -15,8 +15,21 @@ export default class App extends Component {
   render() {
 return(
     <div>
+      <Router>
+
 <Navbar/>
-<News country="in" category = "sports"/>
+
+<Switch>
+          <Route path="/"> <News country="in" category = "general"/>  </Route>
+          <Route path="/business"> <News country="in" category = "business"/>  </Route>
+          <Route path="/entertainment"> <News country="in" category = "entertainment"/>  </Route>
+          <Route path="/health"> <News country="in" category = "health"/>  </Route>
+          <Route path="/science"> <News country="in" category = "science"/>  </Route>
+          <Route path="/technology"> <News country="in" category = "technology"/>  </Route>
+          <Route path="/sports"> <News country="in" category = "sports"/>  </Route>
+        
+        </Switch>
+</Router>
     </div>
 )
 }
