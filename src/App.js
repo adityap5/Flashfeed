48d13919e11e814aae6ec629789935313ea08af7
adityap5 <aditya29pippal@gmail.com>
2023-08-09 22:58:22 +0530

    api

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {
 
 export default class App extends Component {
 pagesize= 9;
-
+// apikey=process.env.REACT_APP_NEWS_API
 // state={
 //   progress:0
 // }
@@ -38,7 +38,7 @@ pagesize= 9;
           <Routes>
             <Route path="/" element={<News key='general' country="in" pagesize={this.pagesize} category="general" />}></Route>
             <Route path="/Business" element={<News key='business' country="in" pagesize={this.pagesize} category="business" />}></Route>
-            <Route path="/Entertainment" element={<News  key='entertainment' pagesize={this.pagesize} country="in" category="entertainment" />}></Route>
+            <Route path="/Entertainment" element={<News key='entertainment' pagesize={this.pagesize} country="in" category="entertainment" />}></Route>
             <Route path="/General" element={<News key='general' country="in" pagesize={this.pagesize} category="general" />}></Route>
             <Route path="/Health" element={<News key='health' country="in" pagesize={this.pagesize} category="health" />}></Route>
             <Route path="/Science" element={<News key='science' country="in" pagesize={this.pagesize} category="science" />}></Route>
